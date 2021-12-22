8a9e5311d2093c148816b883e78dd6350bd699b0
abhay382 <62319333+abhay382@users.noreply.github.com>
2021-12-22 01:37:41 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { TextForm } from './TextForm';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <TextForm/>
+      </div>
+    )
+  }
+}
+
